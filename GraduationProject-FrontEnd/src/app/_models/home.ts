429fc8ba2505d7c028258f84e9281931dfb14ca2
Esraa-a-abdelslam <esraa.a.abdelslam@gmail.com>
2021-06-16 04:36:01 +0200

    create home model

---
+++ GraduationProject-FrontEnd/src/app/_models/home.ts
@@ -0,0 +1,13 @@
+export class Home {
+    constructor(
+        public PhotoName: string = "",
+        public CarName: string = "Car Name",
+        public FuelType: number = 0,
+        public Manual_Automatic: string = "Manual", 
+        public Cylinders: number = 0,
+        public ClassName: string = "Class Name",
+        public Price: number = 0,
+        public Year: number = 0,
+    )
+    {}
+}
