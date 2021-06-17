a66402a167a8176a36046f58676540dae5347753
Esraa-a-abdelslam <esraa.a.abdelslam@gmail.com>
2021-06-18 01:50:52 +0200

    edit home

--- GraduationProject-FrontEnd/src/app/_models/home.ts
+++ GraduationProject-FrontEnd/src/app/_models/home.ts
@@ -1,13 +1,15 @@
 export class Home {
     constructor(
-        public PhotoName: string = "",
-        public CarName: string = "Car Name",
-        public FuelType: number = 0,
-        public Manual_Automatic: string = "Manual", 
-        public Cylinders: number = 0,
-        public ClassName: string = "Class Name",
-        public Price: number = 0,
-        public Year: number = 0,
+        public carDetailsID: number = 0,
+        public transmissionType: string = "",
+        public imageName: string = "",
+        public carType: string = "Car Name",
+        public carName: string = "Car Name",
+        public className: string = "Car Name",
+        public fuel: number = 0,
+        public cylinders: number = 0,
+        public price: number = 0,
+        public year: number = 0,
     )
     {}
 }
--- GraduationProject-FrontEnd/src/app/components/home/home.component.ts
+++ GraduationProject-FrontEnd/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Home } from '../../_models/home';
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from './../../services/home.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  c: Home = new Home();
+  homeDetails: Home[] = [];
+  constructor(
+    public h: HomeService,
+    private ac: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this.ac.params.subscribe((p) => {
+      this.h.getDetailsForHome(p.id).subscribe((a) => {
+        this.homeDetails = a;
+      });
+    });
   }
-
 }
