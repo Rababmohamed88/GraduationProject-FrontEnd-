933a09bed193e7e193cfea0629b19f489e060bf1
ahmedeladlyy <ahmedshababn@gmail.com>
2021-06-19 12:30:21 +0200

    Image gallery added and intergrated :D

--- GraduationProject-FrontEnd/src/app/app.module.ts
+++ GraduationProject-FrontEnd/src/app/app.module.ts
@@ -3,28 +3,31 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RouterModule } from '@angular/router';
-import{FooterComponent} from './components/footer/footer.component'
-import {NavbarComponent} from './components/navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import {HomeComponent} from './components/home/home.component';
-import { CarDetailsComponent } from './components/car-details/car-details.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { ButtonModule } from 'primeng/button';
+import { GalleriaModule } from 'primeng/galleria';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    
+
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    CarDetailsComponent
+    CarDetailsComponent,
+    GalleryComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,9 +36,10 @@ import { CarDetailsComponent } from './components/car-details/car-details.compon
     FormsModule,
     RouterModule,
     NgbModule,
+    ButtonModule,
+    GalleriaModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
